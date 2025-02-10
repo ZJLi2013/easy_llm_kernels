eb88e43c4b92cd89fba724b20ee3ad2eddbad2d8
zjli2013 <leezhengjiang@gmail.com>
2025-02-10 14:52:28 +0800

    add radix decode/extend attn kernels from sglang triton_ops

--- kernels/__init__.py
+++ kernels/__init__.py
@@ -1,7 +1,15 @@
-from .matmul import matmul 
-from .fused_flashattn_v2 import attention 
-from .fused_moe import fused_moe_kernel, fused_experts, fused_moe, fused_topk
 
-__all__ = ["matmul", "attention", "fused_moe_kernel", "fused_moe", "fused_experts", "fused_topk"]
+# matmul ops
+from .matmul import matmul 
+
+## attn ops 
+from .fused_flashattn_v2 import attention 
+from .radix_decode_attn import decode_attention_fwd 
+from .radix_extend_attn import extend_attention_fwd 
+
+## moe ops 
+from .fused_moe import fused_moe_kernel
+
+__all__ = ["matmul", "attention", "decode_attention_fwd", "extend_attention_fwd", "fused_moe_kernel"]
 
 
--- kernels/radix_extent_attn.py
+++
@@ -1,2 +0,0 @@
-# https://github.com/sgl-project/sglang/blob/main/python/sglang/srt/layers/attention/triton_ops/extend_attention.py
-
